718f222ee4bdb9eff1d09bf41e237296888b991a
ofeenee <ofeenee@gmail.com>
2022-09-07 20:02:43 +0300

    set cookie with setHeaders api

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,9 +1,10 @@
 import type { Handle } from '@sveltejs/kit';
 import { redirect, error as serverError } from '@sveltejs/kit';
 import type { CookieSerializeOptions } from 'cookie';
+import * as cookie from 'cookie';
 
 export const handle: Handle = async ({ event, resolve }) => {
-  const { cookies, setHeaders, locals } = event;
+  const { setHeaders, locals } = event;
   try {
     const cookieOptions: CookieSerializeOptions = {
       // domain: 'localhost:3000',
@@ -18,20 +19,31 @@ export const handle: Handle = async ({ event, resolve }) => {
       priority: 'high',
     };
 
-    const valid_token = cookies.get('valid_token');
+    const cookieHeader = event.request.headers.get('cookie');
+    const valid_token = cookie.parse(cookieHeader || '').valid_token;
+    let newCookieString = '';
+
     console.log('valid_token:', valid_token);
 
-    if (!valid_token) cookies.set('valid_token', 'true', cookieOptions);
-
-    if (valid_token === 'false') {
-      cookies.delete('valid_token');
+    if (!valid_token) {
+      newCookieString = cookie.serialize('valid_token', 'true', cookieOptions);
+    }
+    else if (valid_token === 'false') {
+      newCookieString = cookie.serialize('valid_token', '', { ...cookieOptions, expires: new Date(0) });
       // throw serverError(401, 'unauthorized');
     }
-    if (valid_token === 'true') {
-      cookies.set('valid_token', 'false', cookieOptions);
+    else if (valid_token === 'true') {
+      newCookieString = cookie.serialize('valid_token', 'false', cookieOptions);
     }
+    else {
+      throw serverError(500, 'unknown cookie value');
+    }
+
+    setHeaders({ 'set-cookie': newCookieString });
+
   }
   catch (error) {
+    console.log('error:', error);
     // if (error.message === 'unauthorized') cookies.delete('valid_token');
   }
   finally {
